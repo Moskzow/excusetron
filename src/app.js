04df83fa17426fac3845a5e13b13932bc60a3a77
Moskzow <inmoskwetrust@gmail.com>
2021-07-18 01:07:10 +0000

    excusetron is done!

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuseTron();
+  console.log("Is done the excuseTron!");
+};
+
+let excuseTron = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was sleeping"
+  ];
+
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let actionRandom = Math.floor(Math.random() * action.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[whoRandom] +
+    " " +
+    action[actionRandom] +
+    " " +
+    what[whatRandom] +
+    " " +
+    when[whenRandom];
+
+  return excuse;
 };
